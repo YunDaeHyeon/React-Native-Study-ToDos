3996136731a4acff293c2ed9bbaae00892558f3c
DaeHyeon <kayuaao96@gmail.com>
2022-08-13 18:45:26 +0900

    22.08.13 3.6 Delete add

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { theme } from "./colors";
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert } from 'react-native';
 import { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Fontisto } from '@expo/vector-icons';
 
+// AsyncStorage에 저장하기 위한 key 지정
 const STORAGE_KEY = "@toDos";
 
 export default function App() {
@@ -34,10 +36,31 @@ export default function App() {
     const loadToDoItem = await AsyncStorage.getItem(STORAGE_KEY);
     setToDos(JSON.parse(loadToDoItem)); //JSON.parse는 string을 JSON으로 변경한다.
   }
+
+  // To Do 삭제하기
+  const deleteToDo = async (key) => {
+    // 1, 2번째 인자 -> 제목, 내용 / 3번째 인자 -> array 형태의 Button
+    Alert.alert(
+      "Delete To Do", 
+      "Are you sure?",[
+        {text: "Cancel"},
+        // I'm Sure 클릭 시 익명함수를 지정하여 To DO 삭제 진행
+        {
+          text: "I'm Sure", 
+          style: "destructive", // Alert의 style은 iOS만 적용 가능
+          onPress: () => {
+            // To Do를 삭제하기 위해서는 반드시 기존 To Do에 있는 삭제하고자 하는 key를 삭제해야한다.
+            const newToDos = {...toDos}; // 새로운 object를 만든다. 내용은 기존의 toDos로.
+            delete newToDos[key]; // 새로운 object(기존 toDos)에 있는 key를 삭제(delete 키워드)한다.
+            setToDos(newToDos); // 삭제한 뒤 State 변경
+            saveToDos(newToDos);
+        }},
+    ]);
+  }
   // input submut
   const addToDos = async () =>{
     if(text === ""){ // 입력창이 비어있을 때 (text가 비어있을 때)
-      return
+      return;
     }
     // 기존 ToDos에 사용자가 입력한 새로운 ToDo 합치기
     const newToDos = Object.assign({}, toDos, 
@@ -46,6 +69,7 @@ export default function App() {
     await saveToDos(newToDos);
     setText("");
   }
+  console.log(toDos);
   return (
     <View style={styles.container}>
       <StatusBar style="light"/>
@@ -71,6 +95,10 @@ export default function App() {
                 toDos[key].working === working ? (
                 <View style={styles.toDo} key={key}>
                   <Text style={styles.toDoText}>{toDos[key].text}</Text>
+                  {/* 이때, 익명함수를 사용한 이유는 인자(key)를 deleteTodo에 넘겨줘야 하기 때문*/}
+                  <TouchableOpacity onPress={() => deleteToDo(key)}>
+                    <Fontisto name="trash" size={18} color={theme.grey}/>
+                  </TouchableOpacity>
                 </View>
                 ) : null
               ))}
@@ -108,6 +136,9 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   toDoText:{
     color: "white",
@@ -134,4 +165,8 @@ const styles = StyleSheet.create({
   주의사항
   AsyncStorage를 사용하면 해당 API를 사용한 모든 곳에
   async와 await를 지정해야한다.
+
+  Alert API
+  대화창(팝업창) 호출 API
+  사용법 : propmpt() -> iOS 전용, alert() -> 공용
 */
