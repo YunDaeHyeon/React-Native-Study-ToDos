5d70f0beb416056c0cba2cd9c8cf24295b0a7dc8
DaeHyeon <kayuaao96@gmail.com>
2022-08-13 17:10:43 +0900

    22.08.13 3.2 TextInput add

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { theme } from "./colors";
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const onTravleClick = () => setWorking(false);
+  const onWorkClick = () => setWorking(true);
+  const onChangeText = (payload) =>{
+    setText(payload); // RN은 event.target.value와 달리 바로 데이터에 접근이 가능하다.
+  }
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style="light"/>
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={onWorkClick}>
+          <Text style={{...styles.btnText, color: working? "white" : theme.grey}}>Work</Text>
         </TouchableOpacity>
-        <TouchableHighlight activeOpacity={0.5} underlayColor="#FFFFFF" onPress={() => console.log("pressed")}>
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableHighlight>
+        <TouchableOpacity onPress={onTravleClick}>
+          <Text style={{...styles.btnText, color: !working? "white" : theme.grey}}>Travel</Text>
+        </TouchableOpacity>
       </View>
+        <TextInput 
+          style={styles.input}
+          // keyboardType="phone-pad"
+          // returnKeyType="send"
+          // secureTextEntry
+          multiline
+          placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+          onChangeText={onChangeText} // TextInput에 입력된 데이터 가져오기
+          value={text}
+          />
     </View>
   );
 }
@@ -22,7 +40,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.bg,
-    paddingHorizontal: 20, // css에는 없는 속성
+    paddingHorizontal: 20,
   },
   header:{
     justifyContent:"space-between",
@@ -32,19 +50,25 @@ const styles = StyleSheet.create({
   btnText:{
     fontSize: 38,
     fontWeight: "600",
-    color:"white",
+  },
+  input:{
+    backgroundColor: "white",
+    paddingVertical:10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 15,
   },
 });
 
 /*
-  TouchableOpacity (대중적)
-  버튼 클릭 listen 컴포넌트 (View)
-  Touchable(터치하다) / Opacity(투명도, 애니메이션)
-  = 클릭된 요소를 애니메이션으로 투명도 변환
+  TextInput 컴포넌트
+  공식 문서에 매우 다양한 prop이 존재한다.
 
-  TouchableHighlight
-  TouchableOpacity보다 더 많은 옵션을 가지고 있으며
-  대표적으로 클릭된 요소의 투명도를 설정할 수 있거나, 배경색을 변경시킨다.(underlayColor)
-  해당 컴포넌트는 onPress를 설정해야 정상 작동한다. (이는 Opacity에도 가능)
-  투명색 지정 : ex) activeOpacity={0.5}
+  다양한 대표적인 props
+  1. placeholder
+  2. keyboardType (키보드 타입 변경)
+  3. returnKeyType (return 버튼 커스텀)
+  4. secureTextEntry (입력되는 데이터 암호화)
+  5. multiline (한 줄 이상 입력될 때)
 */
